53db7c9cf97df5eae2098d7bc4d25836aa465978
grant thomas <grantwilliamthomas@hotmail.co.uk>
2021-05-13 23:09:27 +0200

    logic for three.js and x y animations

--- src/script.js
+++ src/script.js
@@ -3,6 +3,11 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+//Texture Loader
+const loader = new THREE.TextureLoader()
+const cloud = loader.load('/cloud.png')
+const moon = loader.load('/moon.png')
+
 // Debug
 const gui = new dat.GUI()
 
@@ -13,23 +18,62 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const geometry = new THREE.PlaneGeometry( 1, 1, 200, 200);
+
+const particlesGeometry = new THREE.BufferGeometry;
+const particlesGeometry2 = new THREE.BufferGeometry;
+const particlesCount = 5000
+
+
+const posArray = new Float32Array(particlesCount * 3);
+const posArray2 = new Float32Array(particlesCount * 3);
+
+for(let i = 0; i < particlesCount * 3; i++){
+    // posArray[i] = Math.random()
+    // posArray[i] = Math.random() - 0.5
+    posArray[i] = (Math.random() - 0.5) * (Math.random() * 3)
+    posArray2[i] = (Math.random() - 0.5) * (Math.random() * 3)
+};
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3))
+particlesGeometry2.setAttribute('position', new THREE.BufferAttribute(posArray2, 3))
+
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.PointsMaterial({
+    size: 0.003,
+    color: "yellow"
+})
+
+const PointsMaterial = new THREE.PointsMaterial({
+    size: 0.01,
+    map: cloud,
+    transparent: true,
+    color: 'white',
+    blending: THREE.NormalBlending
+})
+
+const PointsMaterial2 = new THREE.PointsMaterial({
+    size: 0.01,
+    map: moon,
+    transparent: true,
+    color: 'blue',
+    opacity: 0.5
+})
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+const sphere = new THREE.Points(geometry,material)
+const particlesMesh = new THREE.Points(particlesGeometry, PointsMaterial)
+const particlesMesh2 = new THREE.Points(particlesGeometry2, PointsMaterial2)
+scene.add(sphere, particlesMesh, particlesMesh2)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
 pointLight.position.x = 2
 pointLight.position.y = 3
-pointLight.position.z = 4
+pointLight.position.z = 1
 scene.add(pointLight)
 
 /**
@@ -59,10 +103,10 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(50, sizes.width / sizes.height, 0.1, 50)
 camera.position.x = 0
 camera.position.y = 0
-camera.position.z = 2
+camera.position.z = 1
 scene.add(camera)
 
 // Controls
@@ -77,6 +121,38 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor(new THREE.Color('black'), 1)
+
+//Mouse
+
+document.addEventListener('mousemove', animateParticles)
+
+let mouseX = 0
+let mouseY = 0
+
+function animateParticles(e) {
+    mouseY = e.clientY
+    mouseX = e.clientX
+}
+
+const textContent = document.querySelector('.content')
+        console.log(textContent)
+
+        let currentPos = window.pageYOffset;
+        const callDistort = function() {
+            console.log("call distort")
+            const newPos = mouseX
+            const diff = newPos - currentPos;
+            const speed = diff * .2;
+
+            textContent.style.transform = "skewY(" + speed + "deg)";
+            currentPos = newPos;
+
+            requestAnimationFrame(callDistort); 
+
+        };
+
+        callDistort();
 
 /**
  * Animate
@@ -91,6 +167,13 @@ const tick = () =>
 
     // Update objects
     sphere.rotation.y = .5 * elapsedTime
+    sphere.rotation.z = .2 * elapsedTime
+    sphere.rotation.x = .1 * elapsedTime
+    
+    
+    particlesMesh.rotation.x = - mouseY * (elapsedTime * 0.0001)
+    particlesMesh.rotation.y = elapsedTime * 0.0001
+    particlesMesh2.rotation.y = elapsedTime * 0.0001
 
     // Update Orbital Controls
     // controls.update()
